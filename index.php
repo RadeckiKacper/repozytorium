c3f240285ae34fb70d6ff9b7211df16176dcd3d0
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2021-09-06 16:14:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,30 @@
         c
         </footer>
         <aside>
-        d
+        <?php
+        
+        $servername = "172.16.131.125";
+        $username = "z_wer";
+        $password = "gfd";
+        $dbname = "z_wer";
+
+        $conn = new mysqli($servername,$username,$password,$dbname);
+        $sql = "SELECT * from produkty";
+        $result = mysqli_query($conn,$sql);
+
+        echo('<table border="1">');
+        echo('<th>id_klient</th><th>imie</th><th>nazwa</th><th>koszt</th><th>ilosc</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id_produkt'].'</td><td>'.$row['nazwa'].'</td>');
+            echo('</tr>');
+
+        }
+        echo('</table>');
+
+
+
+    ?>
         </aside>
         </main>
         e
